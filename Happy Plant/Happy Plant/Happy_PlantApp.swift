4d66d8d4cbe192fcc3e9ad302ce2294681dad210
jjstapes <joshstapley_89@yahoo.com>
2022-05-10 19:00:43 -0600

    Initial Upload
    Swift code for Happy Plant App

---
+++ Happy Plant/Happy Plant/Happy_PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_PlantApp.swift
+//  Happy Plant
+//
+//  Created by Joshua Stapley on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Happy_PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
